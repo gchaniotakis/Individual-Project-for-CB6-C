3c9e2691ea6ab2082b8934ee6f8c2b1edee644a5
George Chaniotakis <haniotinho@gmail.com>
2019-03-21 20:26:01 +0200

    added class

--- Project Application/File_Access.cs
+++ Project Application/File_Access.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace Project_Application
 {
-    class Program
+    class File_Access
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
