3407879f6d6c20a371c549b3845915addba8ce94
George Chaniotakis <haniotinho@gmail.com>
2019-03-21 20:34:29 +0200

    File Access class

--- Project Application/File_Access.cs
+++ Project Application/File_Access.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using System.IO;
+
 
 namespace Project_Application
 {
-    class File_Access
+    class File_Acess
     {
+
+        static string Path = Properties.Settings.Default.path;
+
+        static internal void NewMessage(Users.Message d)
+        {
+            string textToWrite = $"From:{d.Sender} To:{d.Receiver} At: {d.Datetime} | {d.Content}";
+            string path = Path + $"\\MessageID{d.MessageId}.txt";
+            File.WriteAllText(path, textToWrite);
+        }
+
+        static internal void EditMessage(Users.Message d)
+        {
+            string textToOverwrite = $"From:{d.Sender} To:{d.Receiver} At: {d.Datetime} | {d.Content} *MESSAGE EDITED AT {DateTime.UtcNow}*";
+            string path = Path + $"\\MessageID{d.MessageId}.txt";
+            File.WriteAllText(path, textToOverwrite);
+        }
+
+        static internal void DeleteMessage(Users.Message d)
+        {
+            string path = Path + $"\\MessageID{d.MessageId}.txt";
+            File.Delete(path);
+        }
     }
 }
